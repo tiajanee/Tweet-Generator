bf08611fd201693f5da92874fad7411d4b47cc20
Tia King <tiaking@Timothys-MacBook-Pro.local>
2017-10-26 16:09:41 -0700

    Optimized Speed, Cut approximately 3.555 seconds off run time

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,5 +1,6 @@
 import random
 import sys
+import time
 
 '''
 
@@ -9,30 +10,36 @@ only as long as the number of words that the user inputs.
 '''
 
 with open("/usr/share/dict/words", "r") as myfile:  #opens file
-	word_list = myfile.read()						#stores reading file into word_list
+	word_list = myfile.read().splitlines()						#stores reading file into word_list
 
 def generate_word(number):
+
+
 
 	phrase = ""
 	
 	for i in range(number): #repeats for input number of times
 		
-		lines = word_list.splitlines() #creates list of words and stores them in lines
-		line_number = random.randrange(0, len(lines)) #generates random number between 0 and 
-		word = lines[line_number]  #gets words @ random index  		 #the length of the list
+		line_number = random.randrange(0, len(word_list)) #generates random number between 0 and 
+		word = word_list[line_number]  #gets words @ random index  		  #the length of the list
 
 		if i == number - 1: #if the number is less than the number inputed
 			phrase += (word + "!") #concatenate sentence with new word
 		else:
 			phrase += (word + " ") 
-
+	
 	return phrase
 	
 	
 
 if __name__ == '__main__':
+	time_one = time.time() 
 	input_args = sys.argv[1] 
 	phrase_length = int(input_args) #setting sentence length equal to input length
-
 	phrase = generate_word(phrase_length)
 	print(phrase)
+	final_time = time.time() - time_one
+	print(final_time)
+
+
+
